88d176bba2e475ac94e46807a6a8f5751809c542
irene <irene.frade@alumnos.upm.es>
2024-04-06 17:07:24 +0200

    prueba

--- backend/src/main/java/es/upm/dit/isst/backend/repository/EmpleadoRepository.java
+++
@@ -1,9 +0,0 @@
-package es.upm.dit.isst.backend.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import es.upm.dit.isst.backend.model.*;
-
-public interface EmpleadoRepository extends CrudRepository<EMPLEADO, Long> {
-     
-}
