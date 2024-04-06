1967f3f107f153587dbe1a93f10c4d9d75ea0102
cmcroce <124526514+cmcroce@users.noreply.github.com>
2024-04-06 13:52:02 +0200

    Add files via upload

---
+++ backend/src/main/java/es/upm/dit/isst/backend/repository/EmpresaClienteRepository.java
@@ -0,0 +1,11 @@
+package es.upm.dit.isst.backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import es.upm.dit.isst.backend.model.*;
+
+public interface EmpresaClienteRepository extends CrudRepository<EMPRESACLIENTE, Long>{
+    
+}
