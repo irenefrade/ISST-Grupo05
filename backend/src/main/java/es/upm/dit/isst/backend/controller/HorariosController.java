56cc9f6a8f89604e692486cb503ae7661679c923
cmcroce <124526514+cmcroce@users.noreply.github.com>
2024-04-24 15:15:53 +0200

    Add files via upload

--- backend/src/main/java/es/upm/dit/isst/backend/controller/HorariosController.java
+++ backend/src/main/java/es/upm/dit/isst/backend/controller/HorariosController.java
@@ -9,13 +9,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import es.upm.dit.isst.backend.model.*;
 import es.upm.dit.isst.backend.repository.*;
@@ -61,22 +55,29 @@ public class HorariosController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PutMapping("/horarios/{id}")
-    ResponseEntity<HORARIOS> update(@PathVariable Long id, @RequestBody HORARIOS newHorario) {
-        Optional<HORARIOS> optionalHorario = horariosRepository.findById(id);
-        if (optionalHorario.isPresent()) {
-            HORARIOS horario = optionalHorario.get();
+    ResponseEntity<HORARIOS> update(@RequestBody HORARIOS newHorario, @PathVariable long id) {
+        return horariosRepository.findById(id).map(horario -> {
             horario.setFecha(newHorario.getFecha());
             horario.setHoraEntrada(newHorario.getHoraEntrada());
             horario.setHoraSalida(newHorario.getHoraSalida());
             horario.setMinutosPau(newHorario.getMinutosPau());
             horario.setMinutosTot(Duration.between(newHorario.getHoraEntrada(), newHorario.getHoraSalida()).toMinutes()- newHorario.getMinutosPau());
             horario.setMinutosExt(Duration.between(newHorario.getHoraEntrada(), newHorario.getHoraSalida()).toMinutes()- newHorario.getMinutosPau()-480);
-            horariosRepository.save(horario);
-            return ResponseEntity.ok(horario);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+            horario.setJornada(newHorario.getJornada());
+            return ResponseEntity.ok(horariosRepository.save(horario));
+        }).orElseGet(() -> {
+            newHorario.setId(id);
+            return ResponseEntity.ok(horariosRepository.save(newHorario));
+        });
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
+    @DeleteMapping("/horarios/{id}")
+    ResponseEntity<?> delete(@PathVariable Long id) {
+        return horariosRepository.findById(id).map(horario -> {
+            horariosRepository.delete(horario);
+            return ResponseEntity.ok().build();
+        }).orElse(ResponseEntity.notFound().build());
+    }
 
 }
